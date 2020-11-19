64f34b1b00b17a361f1aba1f4aaf9cc78b20f8b9
Sofia <sofia.navarro.95@gmai.com>
2020-11-18 17:31:36 -0800

    First Week Project

---
+++ index.js
@@ -0,0 +1,33 @@
+var itemPriceShirt = 25
+var itemPricePants = 99
+
+var amountOfMoneyInWalletDad = 160
+var amountOfMoneyInWalletMom = 55
+
+var numberOfFriendsGirls = 3
+var numberOfFriendsBoys = 5
+
+var ageInYearsChild = 13
+var ageInYearsAdult = 30
+
+var firstName1 = 'Sofia'
+var fistName2 = 'Tim'
+
+var lastName1 = 'Navarro'
+var lastName2 = 'Valencia'
+
+var middleInitial1 = 'I'
+var middleInitial2 = 'E'
+
+var moneyLeft = amountOfMoneyInWalletDad - itemPricePants;
+
+var friendYears = ageInYearsAdult / numberOfFriendsBoys;
+
+var fullName = firstName1 +' ' +middleInitial1 + ' ' +lastName1;
+
+console.log('Dad has this money left:', moneyLeft);
+console.log('I have this many friends over the years:', friendYears);
+console.log('This is my full name:', fullName);
+
+
+
